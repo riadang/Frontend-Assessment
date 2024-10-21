eadc2390fa4421a27790fcd4b30c1447130934be
riadang <130978949+riadang@users.noreply.github.com>
2024-10-21 12:16:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".sidenav");
+  var instances = M.Sidenav.init(elems);
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  var elems = document.querySelectorAll(".tabs");
+  var instances = M.Tabs.init(elems);
+});
